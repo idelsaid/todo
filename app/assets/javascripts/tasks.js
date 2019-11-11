06263e2114ed7801ad5ba34f3fe42114dd4fd27a
IdelSaid <idelsaid@gmail.com>
2019-11-11 06:15:15 -0600

    finish

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,16 +2,18 @@ $(function() {
     // The taskHtml method takes in a JavaScript representation
     // of the task and produces an HTML representation using
     // <li> tags
-    function taskHtml(task) {
-      var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
-        " data-id='" + task.id + "'" +
-        checkedStatus +
-        '><label>' +
-         task.title +
-         '</label></div></li>';
+  function taskHtml(task) {
+    var checkedStatus = task.done ? "checked" : "";
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li class="' + liClass + '">' +
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+      " data-id='" + task.id + "'" +
+      checkedStatus +
+      '><label>' +
+       task.title +
+       '</label></div></li>';
 
-      return liElement;
+    return liElement;
     }
 
     // toggleTask takes in an HTML representation of the
@@ -28,7 +30,12 @@ $(function() {
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+      } );
     }
 
     $.get("/tasks").success( function( data ) {
